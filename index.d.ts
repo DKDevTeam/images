96517eb67c697b52f7cb57e6413d2e9c0d117c6d
DeVLasseV <84160130+theSlade@users.noreply.github.com>
2021-07-16 22:57:09 +0200

    Updated

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,4 @@
-
+/**
+ * An API to fetch images and a website for download images
+ */
+declare module "images";
--- src/routes/api.js
+++ src/routes/api.js
@@ -1,18 +1,15 @@
 const router = require("express").Router();
 const path = require("path");
 const fs = require("fs");
-const IMAGE_DATA = path.join(__dirname, "../data/image.data.json");
+const config = require("../data/image.data");
+const IMAGE_DATA = path.join(__dirname, `${config.imagedata}`);
 
 router.get("/", (req, res) => {
   res.send(200);
 });
 
 router.get("/backgrounds", function (req, res, next) {
-  fs.readFile(IMAGE_DATA, function (err, data) {
-    if (err) process.exit(1);
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.json(JSON.parse(data));
-  });
+  res.json(`{background_urls [${config.imagedata}]}`);
 });
 
 module.exports = router;
