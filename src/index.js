86122faea259576f67df391801e00c70e5641a00
DeVLasseV <84160130+theSlade@users.noreply.github.com>
2021-07-21 19:32:01 +0200

    Updated

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-app.set("port", process.env.PORT || 5000);
+app.set("port", process.env.PORT || 8000);
 const imageRouter = require("./routes/_api");
 const config = require("../config.json");
 
@@ -11,6 +12,10 @@ app.get("/image", function (req, res) {
 });
 
 app.use(express.static("src/public"));
+
+app.get("/", function (req, res) {
+  res.send("Hello");
+});
 
 app.listen(app.get("port"), function () {
   console.log("Node app is running on port", app.get("port"));
--- src/routes/_api.js
+++ src/routes/_api.js
@@ -3,7 +3,7 @@ const path = require("path");
 const fs = require("fs");
 const IMAGE_DATA = path.join(__dirname, "../data/images.json");
 
-router.get("/images", function (req, res, next) {
+router.get("/v1/images", function (req, res, next) {
   fs.readFile(IMAGE_DATA, function (err, data) {
     if (err) process.exit(1);
     res.setHeader("Access-Control-Allow-Origin", "*");
